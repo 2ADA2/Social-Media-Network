b57e6a003a18f577648f5271ee6c3692cea93697
2ADA2 <artemada2@gmail.com>
2026-09-07 23:17:04 +0300

    feat: 4o4 page rout

--- src/app/providers/RouterProvider/index.tsx
+++ src/app/providers/RouterProvider/index.tsx
@@ -1,24 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { MainPage } from "@/pages/main";
 import { ROUTES } from "@/shared/config/routes.ts";
-import { ProfilePage } from "@/pages/profile/ui/ProfilePage.tsx";
-import { SignUpPage } from "@/pages/sign-up/ui/SignUpPage.tsx";
-import { SignInPage } from "@/pages/sign-in/ui/SignInPage.tsx";
+import { MainPage } from "@/pages/main";
 import { MainLayout } from "@/app/layouts/MainLayout";
+import { ProfilePage } from "@/pages/profile";
+import { SignUpPage } from "@/pages/sign-up";
+import { SignInPage } from "@/pages/sign-in";
+import { NotFoundPage } from "@/pages/not-found";
 
 export const RouterProvider = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path={ROUTES.HOME} element={<MainPage />} />
-          <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+        <Route element={ <MainLayout/> }>
+          <Route path={ ROUTES.HOME } element={ <MainPage/> }/>
+          <Route path={ ROUTES.PROFILE } element={ <ProfilePage/> }/>
         </Route>
 
-        <Route element={<MainLayout hiddenNav={true} />}>
-          <Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
-          <Route path={ROUTES.SIGNIN} element={<SignInPage />} />
+        <Route element={ <MainLayout hiddenNav={ true }/> }>
+          <Route path={ ROUTES.SIGNUP } element={ <SignUpPage/> }/>
+          <Route path={ ROUTES.SIGNIN } element={ <SignInPage/> }/>
         </Route>
+
+        <Route path={ ROUTES.NOT_FOUND } element={ <NotFoundPage/> }/>
       </Routes>
     </BrowserRouter>
   );
--- src/shared/config/routes.ts
+++ src/shared/config/routes.ts
@@ -1,6 +1,9 @@
 export const ROUTES = {
   HOME: "/",
+  PROFILE: "/profile",
+
   SIGNIN: "/sign-in",
   SIGNUP: "/sign-up",
-  PROFILE: "/profile",
+
+  NOT_FOUND: "*",
 };
