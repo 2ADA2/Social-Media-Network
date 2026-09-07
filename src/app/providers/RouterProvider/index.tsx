11fc480be98f6ea76c71cb56727f06658cb57c0d
2ADA2 <artemada2@gmail.com>
2026-09-07 16:05:21 +0300

    feat: main layout

--- src/app/providers/RouterProvider/index.tsx
+++ src/app/providers/RouterProvider/index.tsx
@@ -4,16 +4,21 @@ import { ROUTES } from "@/shared/config/routes.ts";
 import { ProfilePage } from "@/pages/profile/ui/ProfilePage.tsx";
 import { SignUpPage } from "@/pages/sign-up/ui/SignUpPage.tsx";
 import { SignInPage } from "@/pages/sign-in/ui/SignInPage.tsx";
+import { MainLayout } from "@/app/layouts/MainLayout";
 
 export const RouterProvider = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ROUTES.HOME} element={<MainPage/>}/>
-        <Route path={ROUTES.PROFILE} element={<ProfilePage/>}/>
+        <Route element={<MainLayout />}>
+          <Route path={ROUTES.HOME} element={<MainPage />} />
+          <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+        </Route>
 
-        <Route path={ROUTES.SIGNUP} element={<SignUpPage/>}/>
-        <Route path={ROUTES.SIGNIN} element={<SignInPage/>}/>
+        <Route element={<MainLayout hiddenNav={true} />}>
+          <Route path={ROUTES.SIGNUP} element={<SignUpPage />} />
+          <Route path={ROUTES.SIGNIN} element={<SignInPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
