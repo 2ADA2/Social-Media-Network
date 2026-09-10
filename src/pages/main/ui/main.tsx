4ebb845702f8d5a6f21c0b2ec6a35b9eed207243
2ADA2 <artemada2@gmail.com>
2026-09-10 22:42:03 +0300

    feat/theme-switcher

--- src/pages/main/ui/main.tsx
+++ src/pages/main/ui/main.tsx
@@ -1,21 +1,14 @@
 import "./main.css";
 import { Header } from "@/widgets/header";
 import { Footer } from "@/widgets/footer";
-import { Toggle } from "@/shared/ui/Toggle";
-import { useEffect, useState } from "react";
-
+import { ThemeSwitcher } from "@/features/ui/ThemeSwitcher";
 
 export const MainPage = () => {
-  const [checked, setChecked] = useState(false);
-
-  useEffect(() => {
-
-  }, [checked]);
   return (
     <>
       <Header/>
       <main className="main">
-        <Toggle checked={checked} onChange={() => setChecked(!checked)}/>
+        <ThemeSwitcher/>
       </main>
       <Footer/>
     </>
