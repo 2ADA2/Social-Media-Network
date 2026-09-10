d23af9aa28233391bc3e1e20ae131bb0990dcbab
2ADA2 <artemada2@gmail.com>
2026-09-10 22:27:55 +0300

    feat/toggle

--- src/pages/main/ui/main.tsx
+++ src/pages/main/ui/main.tsx
@@ -1,14 +1,21 @@
 import "./main.css";
 import { Header } from "@/widgets/header";
 import { Footer } from "@/widgets/footer";
+import { Toggle } from "@/shared/ui/Toggle";
+import { useEffect, useState } from "react";
 
 
 export const MainPage = () => {
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+
+  }, [checked]);
   return (
     <>
       <Header/>
       <main className="main">
-        MAIN
+        <Toggle checked={checked} onChange={() => setChecked(!checked)}/>
       </main>
       <Footer/>
     </>
