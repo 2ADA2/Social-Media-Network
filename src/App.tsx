78e369de7a109ca0a2a6a14386ddbcfac08fb9d6
2ADA2 <artemada2@gmail.com>
2026-09-14 22:36:22 +0300

    feat: auto choosing theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { MainPage } from "./pages/main";
 import "../global.css";
+import { useLayoutEffect } from "react";
 
 function App() {
+  useLayoutEffect(() => {
+    const isDark = localStorage.getItem("theme") === "dark";
+    document.documentElement.setAttribute("data-theme", isDark ? "dark" : "light");
+  }, []);
+
   return <MainPage/>;
 }
 
