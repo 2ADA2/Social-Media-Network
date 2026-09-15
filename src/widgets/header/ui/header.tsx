5a827e2128f91736c0da42102819316020796603
2ADA2 <artemada2@gmail.com>
2026-09-16 02:24:24 +0300

    feat/navbar-mobile

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -3,18 +3,26 @@ import SidekickLogo from "@/shared/assets/icons/sidekick-logo.svg?react";
 import { useState } from "react";
 import { BurgerMenu } from "@/widgets/header/ui/burgerMenu/burgerMenu.tsx";
 import { AuthButtons } from "@/widgets/header/ui/authButtons/authButtons.tsx";
+import { NavBar } from "@/widgets/navbar/ui/NavBar.tsx";
 
 export const Header = () => {
-  const [auth] = useState<boolean>(true); // useAuth hook ( useContext ) in future
+    const [auth] = useState<boolean>(true); // useAuth hook ( useContext ) in future
+    const [isNavBar, setIsNavBar] = useState(false);
 
-  return (
-    <header className="header">
-      <div className="headerLogo">
-        <SidekickLogo/>
-        <span>sidekick</span>
-      </div>
+    const setNavBar = () => {
+      setIsNavBar(!isNavBar);
+    };
 
-      { auth ? <BurgerMenu/> : <AuthButtons/> }
-    </header>
-  );
-};
+    return (
+      <header className="header">
+        <div className="headerLogo">
+          <SidekickLogo/>
+          <span>sidekick</span>
+        </div>
+
+        { auth ? <BurgerMenu onClick={ setNavBar }/> : <AuthButtons/> }
+        { isNavBar && <NavBar setNavBar={ () => setNavBar() }/> }
+      </header>
+    );
+  }
+;
