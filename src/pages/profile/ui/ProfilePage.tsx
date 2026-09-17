96158ae5027e982c9451ed7d04823d625e1d5d5e
2ADA2 <artemada2@gmail.com>
2026-09-18 02:51:09 +0300

    feat: profile-page-init

--- src/pages/profile/ui/ProfilePage.tsx
+++ src/pages/profile/ui/ProfilePage.tsx
@@ -1,6 +1,9 @@
 import "./ProfilePage.css";
 import { Tabs } from "@/shared/ui/Tabs";
 import { useState } from "react";
+import { ThemeSwitcher } from "@/features/ui/ThemeSwitcher";
+import { Button } from "@/shared/ui/Button";
+import { EditProfile } from "@/widgets/editProfile";
 
 const TABS = [
   { id: 'info', label: 'Profile Info' },
@@ -9,12 +12,32 @@ const TABS = [
 
 export const ProfilePage = () => {
   const [activeTab, setActiveTab] = useState('info');
-  const [showMessage, setShowMessage] = useState(true);
+
+  const logout = () => {
+    console.log("Logout");
+  };
 
   return (
     <div className='profile-page'>
+      <h1 hidden>Edit profile</h1>
+
       <div className='tabs-container'>
         <Tabs tabs={ TABS } activeTab={ activeTab } onChange={ setActiveTab }/>
+      </div>
+
+      <div className='profile-column'>
+        <section>
+          <h2>Preferences</h2>
+          <ThemeSwitcher/>
+        </section>
+        <section>
+          <h2>Actions</h2>
+          <Button onClick={ logout }>Logout</Button>
+        </section>
+      </div>
+
+      <div className='profile-column'>
+        <EditProfile/>
       </div>
     </div>
   );
---
+++ src/widgets/editProfile/index.ts
@@ -0,0 +1,2 @@
+export { EditProfile } from "./ui/EditProfile.tsx";
+
