40508b2a6b545aa4e68cf307b3359ef43f3d0011
2ADA2 <artemada2@gmail.com>
2026-09-18 11:17:54 +0300

    ref: final refactor sign in sign up

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,20 +1,21 @@
-import React, { forwardRef, type InputHTMLAttributes } from "react";
+import { type InputHTMLAttributes, type ReactNode, type Ref } from "react";
 import "./Input.css";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  icon?: React.ReactNode;
+  icon?: ReactNode;
+  ref?: Ref<HTMLInputElement>;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ icon, label, ...props }, ref) => {
+export const Input = ({ icon, label, ref, ...props }: InputProps) => {
   return (
-    <label className='input-label'>
+    <label className="input-label">
       <div>
-        { icon && <div className="input-icon">{ icon }</div> }
-        <span>{ label }</span>
+        {icon && <div className="input-icon">{icon}</div>}
+        <span>{label}</span>
       </div>
 
-      <input ref={ ref } { ...props }/>
+      <input ref={ref} {...props} />
     </label>
   );
-});
+};
