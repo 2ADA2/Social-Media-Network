3e7309f8964d6657d70d48b78cc0275497e6ee11
2ADA2 <artemada2@gmail.com>
2026-09-18 12:29:56 +0300

    merged profile page

--- src/pages/profile/ui/ProfilePage.tsx
+++ src/pages/profile/ui/ProfilePage.tsx
@@ -1,9 +1,6 @@
 import "./ProfilePage.css";
 import { Tabs } from "@/shared/ui/Tabs";
 import { useState } from "react";
-import { ProfileInfo } from "@/pages/profile/ui/ProfileInfo/ProfileInfo.tsx";
-import { Statistics } from "@/pages/profile/ui/Statistics/Statistics.tsx";
-import { useLocation } from "react-router-dom";
 
 const TABS = [
   { id: 'info', label: 'Profile Info' },
@@ -11,25 +8,14 @@ const TABS = [
 ];
 
 export const ProfilePage = () => {
-  const location = useLocation();
-
-  const [activeTab, setActiveTab] = useState(() => {
-    const params = new URLSearchParams(location.search);
-    return params.get('tab') === 'stats' ? 'stats' : 'info';
-  });
-
+  const [activeTab, setActiveTab] = useState('info');
+  const [showMessage, setShowMessage] = useState(true);
 
   return (
     <div className='profile-page'>
-      <h1 hidden>Edit profile</h1>
-
       <div className='tabs-container'>
         <Tabs tabs={ TABS } activeTab={ activeTab } onChange={ setActiveTab }/>
       </div>
-
-      { activeTab === 'info' && <ProfileInfo/> }
-      { activeTab === 'stats' && <Statistics/> }
-
     </div>
   );
 };
