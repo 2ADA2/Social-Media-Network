7d06d65f2c459fee5b60198e02a8c735a14a1151
2ADA2 <artemada2@gmail.com>
2026-09-18 11:14:18 +0300

    ref: sign in / sign up forms refactor

--- src/pages/signin/ui/SignIn.tsx
+++ src/pages/signin/ui/SignIn.tsx
@@ -1,44 +1,54 @@
+import { type ChangeEvent, type SubmitEvent, useState } from 'react';
 import { Input } from "@/shared/ui/Input";
+import { Button } from "@/shared/ui/Button";
 import MailIcon from '@/shared/assets/icons/mail.svg?react';
 import EyeIcon from '@/shared/assets/icons/eye.svg?react';
-import { useRef } from "react";
 import './SignIn.css';
-import { Button } from "@/shared/ui/Button";
 
 export const SignIn = () => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
   return (
-    <section className='auth'>
+    <section className="auth">
       <h1>Sign in into an account</h1>
       <p>
-        Enter your email and password <br/>
+        Enter your email and password <br />
         to sign in into this app
       </p>
 
-      <form className='auth-form'>
+      <form className="auth-form" onSubmit={handleSubmit}>
         <Input
-          label='Email'
-          type='email'
-          placeholder='Enter email'
-          icon={ <MailIcon/> }
-          ref={ inputRef }
+          label="Email"
+          type="email"
+          placeholder="Enter email"
+          icon={<MailIcon />}
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          required
         />
         <Input
-          label='Password'
-          type='password'
-          placeholder='Enter password'
-          icon={ <EyeIcon/> }
-          ref={ inputRef }
+          label="Password"
+          type="password"
+          placeholder="Enter password"
+          icon={<EyeIcon />}
+          value={password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          required
         />
-        <Button>
+        <Button type="submit">
           Sign in
         </Button>
       </form>
-      <p className='auth-form-footer'>
-        Forgot to create an account?{ ' ' }
-        <a href='/sign-up'>
-          Sign up
-        </a>
+
+      <p className="auth-form-footer">
+        Forgot to create an account?{' '}
+        <a href="/sign-up">Sign up</a>
       </p>
     </section>
   );
