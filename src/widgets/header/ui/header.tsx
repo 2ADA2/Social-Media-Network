a91cdb70d1975668c5dcbf1ee05ba9b784f7d0c1
2ADA2 <artemada2@gmail.com>
2026-09-19 16:28:20 +0300

    fix: header nav hidden

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,18 +1,41 @@
 import "./header.css";
 import SidekickLogo from "@/shared/assets/icons/sidekick-logo.svg?react";
-import { type ReactNode, useState } from "react";
+import { useState } from "react";
 import { BurgerMenu } from "@/widgets/header/ui/burgerMenu/burgerMenu.tsx";
 import { AuthButtons } from "@/widgets/header/ui/authButtons/authButtons.tsx";
 import { NavBar } from "@/widgets/navbar";
 import { UserNav } from "@/entities/UserNav";
 
+export interface HeaderProps {
+  hiddenNav?: boolean;
+}
 
-export const Header = () => {
+export const Header = ({ hiddenNav = false }: HeaderProps) => {
     const [auth] = useState<boolean>(true); // useAuth hook ( useContext ) in future
     const [isNavBar, setIsNavBar] = useState(false);
 
     const setNavBar = () => {
       setIsNavBar(!isNavBar);
+    };
+
+    const hideNav = () => {
+      if (hiddenNav) {
+        return <div></div>;
+      }
+
+      return (
+        <>
+          <div className='mobile-nav'>
+            <BurgerMenu onClick={ setNavBar }/>
+          </div>
+
+          <div className='desktop-nav'>
+            { auth ? <UserNav/> : <AuthButtons/> }
+          </div>
+
+          { isNavBar && <NavBar setNavBar={ () => setNavBar() }/> }
+        </>
+      );
     };
 
     return (
@@ -22,17 +45,7 @@ export const Header = () => {
           <span>sidekick</span>
         </div>
 
-
-        <div className='mobile-nav'>
-          <BurgerMenu onClick={ setNavBar }/>
-        </div>
-
-        <div className='desktop-nav'>
-          {auth ? <UserNav/>: <AuthButtons/>}
-        </div>
-
-
-        { isNavBar && <NavBar setNavBar={ () => setNavBar() }/> }
+        { hideNav() }
       </header>
     );
   }
