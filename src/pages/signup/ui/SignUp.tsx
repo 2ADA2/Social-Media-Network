5b7a1913fff516e9883b472f62611780e6d05732
2ADA2 <artemada2@gmail.com>
2026-09-19 14:18:59 +0300

    fix: sign up fix 2

--- src/pages/signup/ui/SignUp.tsx
+++ src/pages/signup/ui/SignUp.tsx
@@ -23,49 +23,51 @@ export const SignUp = () => {
   };
 
   return (
-    <section className='sign-up'>
-      <div className='sign-up-header'>
-        <h1>Create an account</h1>
-        <p>
-          Enter your email and password <br/>
-          to sign up for this app
-        </p>
-      </div>
-
-      <form className='auth-form' onSubmit={ handleSubmit }>
-        <div className='input-container'>
-          <Input
-            label="Email"
-            type="email"
-            placeholder="Enter email"
-            icon={ <MailIcon/> }
-            value={ email }
-            onChange={ changeEmail }
-            required
-          />
-          <Input
-            label="Password"
-            type="password"
-            placeholder="Enter password"
-            icon={ <EyeIcon/> }
-            value={ password }
-            onChange={ changePassword }
-            required
-          />
+    <div className="auth-container">
+      <section className='sign-up'>
+        <div className='sign-up-header'>
+          <h1>Create an account</h1>
+          <p>
+            Enter your email and password <br/>
+            to sign up for this app
+          </p>
         </div>
-        <Button type='submit'>
-          Sign up
-        </Button>
-      </form>
 
-      <small>By clicking continue, you agree to our <b>Terms of Service</b> and <b>Privacy Policy</b></small>
+        <form className='auth-form' onSubmit={ handleSubmit }>
+          <div className='input-container'>
+            <Input
+              label="Email"
+              type="email"
+              placeholder="Enter email"
+              icon={ <MailIcon/> }
+              value={ email }
+              onChange={ changeEmail }
+              required
+            />
+            <Input
+              label="Password"
+              type="password"
+              placeholder="Enter password"
+              icon={ <EyeIcon/> }
+              value={ password }
+              onChange={ changePassword }
+              required
+            />
+          </div>
+          <Button type='submit'>
+            Sign up
+          </Button>
+        </form>
 
-      <p className='auth-form-footer'>
-        Already have an account?{ ' ' }
-        <a href='/sign-in'>
-          Sign in
-        </a>
-      </p>
-    </section>
+        <small>By clicking continue, you agree to our <b>Terms of Service</b> and <b>Privacy Policy</b></small>
+
+        <p className='auth-form-footer'>
+          Already have an account?{ ' ' }
+          <a href='/sign-in'>
+            Sign in
+          </a>
+        </p>
+      </section>
+    </div>
   );
 };
