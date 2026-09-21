fba9e13c90c548d10a2029a1a186dafeaa7c7089
2ADA2 <artemada2@gmail.com>
2026-09-21 05:09:09 +0300

    fix: aside h2 and aside auth

--- src/pages/main/ui/main.tsx
+++ src/pages/main/ui/main.tsx
@@ -3,11 +3,14 @@ import { Post } from "@/entities/post";
 import { CreatePost } from "@/features/create-post/ui/create-post.tsx";
 import { MainSidebar } from "@/widgets/main-sidebar";
 import { POSTS, SUGGESTED_COMMUNITIES, SUGGESTED_PEOPLE } from "@/pages/main/ui/data.ts";
+import { useAuth } from "@/features/auth";
 
 
 
 
 export const MainPage = () => {
+  const isAuth = useAuth();
+
   return (
     <div className='main-page'>
       <CreatePost/>
@@ -17,8 +20,12 @@ export const MainPage = () => {
       </div>
 
       <div className={'recommend-sidebars'}>
-      <MainSidebar title='Suggested people' users={SUGGESTED_PEOPLE}/>
-      <MainSidebar title='Communities you might like' users={SUGGESTED_COMMUNITIES}/>
+        {isAuth && (
+          <>
+            <MainSidebar title='Suggested people' users={ SUGGESTED_PEOPLE }/>
+            <MainSidebar title='Communities you might like' users={SUGGESTED_COMMUNITIES}/>
+          </>
+        )}
       </div>
     </div>
   );
