f0f29f331cddb28afbb7d33bca2c059d2f7ce964
2ADA2 <artemada2@gmail.com>
2026-09-21 23:45:10 +0300

    feat: info on focus

--- src/shared/ui/TextArea/index.tsx
+++ src/shared/ui/TextArea/index.tsx
@@ -1,20 +1,31 @@
 import { type InputHTMLAttributes, type ReactNode } from "react";
+import InfoIcon from "@/shared/assets/icons/info-filled.svg?react";
 import "./textarea.css";
 
 interface InputProps extends InputHTMLAttributes<HTMLTextAreaElement> {
   label?: string;
   icon?: ReactNode;
+  info?: string;
 }
 
-export const TextArea = ({ icon, label, ...props }: InputProps) => {
+export const TextArea = ({ icon, label, info, ...props }: InputProps) => {
+
+
   return (
     <label className="textarea-label">
       <div>
-        {icon && <div className="textarea-icon">{icon}</div>}
-        <span>{label}</span>
+        { icon && <div className="textarea-icon">{ icon }</div> }
+        <span>{ label }</span>
       </div>
 
-      <textarea {...props} />
+      <textarea { ...props } />
+
+      { info &&
+          <div className='info'>
+              <InfoIcon className='ignore'/>
+              <small>{ info }</small>
+          </div>
+      }
     </label>
   );
 };
