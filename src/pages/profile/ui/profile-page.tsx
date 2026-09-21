9ddbd5437bf0180d4d257fceba67eb881d0f8640
2ADA2 <artemada2@gmail.com>
2026-09-21 15:41:24 +0300

    fix/save profile page tab

--- src/pages/profile/ui/profile-page.tsx
+++ src/pages/profile/ui/profile-page.tsx
@@ -1,9 +1,8 @@
 import "./profile-page.css";
 import { Tabs } from "@/shared/ui/Tabs";
-import { useState } from "react";
 import { ProfileInfo } from "@/pages/profile/ui/profile-info/profile-info.tsx";
 import { Statistics } from "@/pages/profile/ui/statistics/statistics.tsx";
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const TABS = [
   { id: 'info', label: 'Profile Info' },
@@ -11,20 +10,19 @@ const TABS = [
 ];
 
 export const ProfilePage = () => {
-  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const activeTab = searchParams.get('tab') === 'stats' ? 'stats' : 'info';
 
-  const [activeTab, setActiveTab] = useState(() => {
-    const params = new URLSearchParams(location.search);
-    return params.get('tab') === 'stats' ? 'stats' : 'info';
-  });
-
+  const handleTabChange = (tab: string) => {
+    setSearchParams({ tab });
+  };
 
   return (
     <div className='profile-page'>
       <h1 hidden>Edit profile</h1>
 
       <div className='tabs-container'>
-        <Tabs tabs={ TABS } activeTab={ activeTab } onChange={ setActiveTab }/>
+        <Tabs tabs={ TABS } activeTab={ activeTab } onChange={ handleTabChange }/>
       </div>
 
       { activeTab === 'info' && <ProfileInfo/> }
