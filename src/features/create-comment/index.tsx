7f191212b86e05b2ba97f915bffcbf9ddf3f3b82
2ADA2 <artemada2@gmail.com>
2026-09-21 05:23:34 +0300

    feat: craete-comment

---
+++ src/features/create-comment/index.tsx
@@ -0,0 +1,26 @@
+import { TextArea } from "@/shared/ui/TextArea";
+import { Button } from "@/shared/ui/Button";
+import PenIcon from "@/shared/assets/icons/pen.svg?react";
+import { type ChangeEvent, useState } from "react";
+
+export const CreateComment = () => {
+  const [comment, setComment] = useState('');
+
+  const changeComment = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  return (
+    <div className='add-comment-section'>
+      <TextArea
+        icon={ <PenIcon/> }
+        label='Add a comment'
+        placeholder='Write a comment...'
+        onChange={ changeComment }
+        value={ comment }
+      />
+      <Button>Add a comment</Button>
+    </div>
+
+  );
+};
