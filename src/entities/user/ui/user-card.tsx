4720a8320d86bd2423f5fa5387beb36b9c52eca4
2ADA2 <artemada2@gmail.com>
2026-09-21 04:26:58 +0300

    feat: user card and sidebar

---
+++ src/entities/user/ui/user-card.tsx
@@ -0,0 +1,20 @@
+import { Avatar } from "@/shared/ui/Avatar/Avatar.tsx";
+import "./user-card.css";
+
+export interface UserCardProps {
+  avatarUrl: string;
+  title: string;
+  subtitle: string;
+}
+
+export const UserCard = ({ avatarUrl, title, subtitle }: UserCardProps) => {
+  return (
+    <div className="user-card">
+      <Avatar src={ avatarUrl } size={ 48 }/>
+      <div className="user-card-info">
+        <div className="user-card-title">{title}</div>
+        <div className="user-card-subtitle">{subtitle}</div>
+      </div>
+    </div>
+  );
+};
