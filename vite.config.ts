5b848e0e132d0e80ec2b68f72d0716e52fe89204
2ADA2 <artemada2@gmail.com>
2026-09-23 23:15:15 +0300

    fix: varcel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/Social-Media-Network',
+  base: '/',
   resolve: {
     alias: {
       "@": resolve(import.meta.dirname, 'src'),
