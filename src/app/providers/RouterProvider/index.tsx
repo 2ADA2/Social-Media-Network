0916de88b7be7e46cd112e74f915945461e72c13
2ADA2 <artemada2@gmail.com>
2026-09-25 00:05:25 +0300

    feat: loader

--- src/app/providers/RouterProvider/index.tsx
+++ src/app/providers/RouterProvider/index.tsx
@@ -5,12 +5,12 @@ import { MainLayout } from "../../layouts/main-layout";
 import { useAuth } from "@/entities/user/model/use-auth.tsx";
 import { ProtectedRoute } from "./ProtectedRoute.tsx";
 import { NotFound } from "@/pages/not-found";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
+import { Loader } from "@/shared/ui/loader";
 
 const SignIn = lazy(() => import('@/pages/signin'));
 const SignUp = lazy(() => import('@/pages/signup'));
 const ProfilePage = lazy(() => import('@/pages/profile'));
-
 
 export const RouterProvider = () => {
   const isAuth = useAuth();
@@ -20,16 +20,29 @@ export const RouterProvider = () => {
       <Routes>
         <Route element={ <MainLayout/> }>
           <Route element={ <ProtectedRoute condition={ isAuth } route={ ROUTES.SIGNIN }/> }>
-            <Route path={ ROUTES.PROFILE } element={ <ProfilePage/> }/>
+            <Route path={ROUTES.PROFILE} element={
+              <Suspense fallback={ <Loader/> }>
+                <ProfilePage/>
+              </Suspense>
+            }/>
           </Route>
 
           <Route path={ ROUTES.HOME } element={ <MainPage/> }/>
         </Route>
 
+        // auth
         <Route element={ <MainLayout hiddenNav={ true }/> }>
           <Route element={ <ProtectedRoute condition={ !isAuth } route={ ROUTES.HOME }/> }>
-            <Route path={ ROUTES.SIGNUP } element={ <SignUp/> }/>
-            <Route path={ ROUTES.SIGNIN } element={ <SignIn/> }/>
+            <Route path={ ROUTES.SIGNUP } element={
+              <Suspense fallback={ <Loader/> }>
+                <SignUp/>
+              </Suspense>
+            }/>
+            <Route path={ ROUTES.SIGNIN } element={
+              <Suspense fallback={ <Loader/> }>
+                <SignIn/>
+              </Suspense>
+            }/>
           </Route>
         </Route>
 
